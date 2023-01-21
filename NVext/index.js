2568bea54a7971fe7223b8b7c6a7f7a43c419412
Ryan Diep <ryandiep5@gmail.com>
2023-01-21 02:54:22 -0500

    created limit on number of array items

--- NVext/index.js
+++ NVext/index.js
@@ -1,17 +1,19 @@
 window.onload = function () {
     var startTime = Date.now() - (90 * 24 * 60 * 60 * 1000);
     var searchQueries = [];
+    var counter = 0;
     chrome.history.search({
         text: '',
         startTime: startTime,
         maxResults: 0
     }, function (historyItems) {
         for (var i = 0; i < historyItems.length; i++) {
-            if (historyItems[i].url.indexOf("q=") > -1) {
+            if (historyItems[i].url.indexOf("q=") > -1 && counter < 2000) {
                 var url = historyItems[i].url;
                 var queryString = url.substring(url.indexOf("q=") + 2);
                 var searchQuery = queryString.split("&")[0];
                 searchQueries.push(searchQuery.split("+").join(" "));
+                counter++; 
             }
         }
     });
